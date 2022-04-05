e038f1ac5812656b9ce50989f9b8eab123722154
Daniel Yeh <dyeh1989@gmail.com>
2022-04-05 07:59:25 +0000

    Listen for 'submit' events on the journal entry form... #6 (All subtasks)

--- js/data.js
+++ js/data.js
@@ -6,3 +6,31 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $titleInput = document.querySelector('#title');
+var $photoURL = document.querySelector('#photo-url');
+var $notesInput = document.querySelector('#notes');
+var $image = document.querySelector('img');
+
+var $motherIsListening = document.querySelector('form');
+$motherIsListening.addEventListener('submit', recordAllInputs);
+
+function recordAllInputs(event) {
+  var newObject = {
+    EntryID: data.nextEntryId,
+    Title: $titleInput.value,
+    PhotoURL: $photoURL.value,
+    Notes: $notesInput.value
+  };
+  data.nextEntryId = ++data.nextEntryId;
+  data.entries.unshift(newObject);
+
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('JS-local-storage', dataJSON);
+
+  event.preventDefault();
+  $photoURL.value = '';
+  $titleInput.value = '';
+  $notesInput.value = '';
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
